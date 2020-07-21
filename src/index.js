e7bb9fed17cf7e4b89e6a8d7ab437b1216d00895
Arti Mathanda <arti.mathanda@simplybusiness.co.uk>
2020-07-21 21:59:02 +0100

    import tachyons

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
