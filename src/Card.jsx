5851d095dd8114b302acaf7245d4b57571062abd
Arti Mathanda <arti.mathanda@simplybusiness.co.uk>
2020-07-21 22:53:03 +0100

    uses props in Card

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, email, id}) => {
+
   return (
-    <div>
-      <img src='https://robohash.org/test?200x200' alt="robot"/>
-      <h2>Janvi </h2>
-      <p>jdoe@gmail.com</p>
+    <div className="tc bg-green dib br3 shadow-5 pa3 ma2 grow">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robot"/>
+      <h2>{name} </h2>
+      <p>{email}m</p>
     </div>
   )
 }
