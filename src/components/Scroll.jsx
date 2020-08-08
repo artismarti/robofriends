2e7598cb82e12a569eadf65746b56796598b0d7c
Arti Mathanda <arti.mathanda@simplybusiness.co.uk>
2020-08-08 18:48:40 +0100

    Updates Scroll

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Scroll = ({children}) => {
   return (
-    <div style={{overflowY: 'scroll', height:'800px'}}>
+    <div style={{overflowY: 'scroll',
+     height:'800px'}}>
       {children}
     </div>
   )
